2d45ecd3e916b31965e3a8b120803c90e6ef10c1
Elwin Stephan <elwin.stephan@gmail.com>
2021-11-24 15:17:28 +0100

    autoformat

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,10 @@
 """Template of an empty global controller"""
 import argparse
+
+import numpy as np
 from p4utils.utils.helper import load_topo
 from p4utils.utils.sswitch_thrift_API import SimpleSwitchThriftAPI
-import numpy as np
+
 
 class Controller(object):
 
@@ -41,7 +43,7 @@ class Controller(object):
         prefix = "/32"
 
         # Create arrays to save number of shortest paths and next hops
-        shortest_path_num      = np.zeros((len(switches_list),len(switches_list)),dtype=int)
+        shortest_path_num = np.zeros((len(switches_list), len(switches_list)), dtype=int)
         shortest_path_next_hop = []
 
         # Compute shortest paths
@@ -51,9 +53,9 @@ class Controller(object):
                 dst_next_hops = []
                 src_ind = switches_list.index(i)
                 dst_ind = switches_list.index(j)
-                shortest_path = self.topo.get_shortest_paths_between_nodes(str(i),str(j))
+                shortest_path = self.topo.get_shortest_paths_between_nodes(str(i), str(j))
                 for k in range(len(shortest_path)):
-                    if (len(shortest_path[k]) > 1):
+                    if len(shortest_path[k]) > 1:
                         dst_next_hops.append(shortest_path[k][1])
                     else:
                         dst_next_hops.append(shortest_path[k][0])
@@ -62,7 +64,7 @@ class Controller(object):
             shortest_path_next_hop.append(src_next_hops)
 
         # Populate tables
-        src_sw=0
+        src_sw = 0
         ecmp_group_id = 0
         max_group_id = 0
 
@@ -73,15 +75,15 @@ class Controller(object):
             next_hops_save = []
 
             direct_hosts = self.topo.get_hosts_connected_to(str(switches_list[src_sw]))
-            #print(direct_hosts)
+            # print(direct_hosts)
 
             for host in direct_hosts:
                 host_ip = self.topo.get_host_ip(host) + prefix
                 host_mac = self.topo.get_host_mac(host)
-                interf_info = self.topo.get_node_intfs(fields=['node_neigh','port'])
+                interf_info = self.topo.get_node_intfs(fields=['node_neigh', 'port'])
                 interf_info_sw = interf_info[str(switches_list[src_sw])]
                 for i in interf_info_sw:
-                    if (interf_info_sw[i][0] == host):
+                    if interf_info_sw[i][0] == host:
                         host_port = interf_info_sw[i][1]
                         break
                 # Add table entries for directly connected hosts
@@ -90,21 +92,21 @@ class Controller(object):
             for dst_sw in range(len(switches_list)):
 
                 # If src and dst switches are the same
-                if (src_sw == dst_sw):
+                if src_sw == dst_sw:
                     continue
 
                 # If src and dst switches are different
                 else:
 
                     # If single shortest path
-                    if (shortest_path_num[src_sw][dst_sw] == 1):
+                    if shortest_path_num[src_sw][dst_sw] == 1:
 
                         # Find next hop port
                         next_hop = shortest_path_next_hop[src_sw][dst_sw][0]
-                        interf_info = self.topo.get_node_intfs(fields=['node_neigh','port'])
+                        interf_info = self.topo.get_node_intfs(fields=['node_neigh', 'port'])
                         interf_info_sw = interf_info[str(switches_list[src_sw])]
                         for i in interf_info_sw:
-                            if (interf_info_sw[i][0] == next_hop):
+                            if interf_info_sw[i][0] == next_hop:
                                 next_hop_port = interf_info_sw[i][1]
                                 break
                         # Find ip and mac addr of dst host
@@ -122,7 +124,7 @@ class Controller(object):
                         no_next_hops = shortest_path_num[src_sw][dst_sw]
 
                         # Save next hops for comparison later
-                        if (len(next_hops_save) == 0):
+                        if len(next_hops_save) == 0:
                             first_next_hops = []
                             for nhop in range(no_next_hops):
                                 first_next_hops.append(shortest_path_next_hop[src_sw][dst_sw][nhop])
@@ -137,7 +139,7 @@ class Controller(object):
                                 current_next_hops_save.append(shortest_path_next_hop[src_sw][dst_sw][nhop])
                             # Compare with the existing ecmp_group_ids
                             for group in range(len(next_hops_save)):
-                                if (set(next_hops_save[group]) == set(current_next_hops_save)):
+                                if set(next_hops_save[group]) == set(current_next_hops_save):
                                     ecmp_group_id = group
                                     exists = True
                                     break
@@ -145,7 +147,7 @@ class Controller(object):
                                     continue
 
                             # Create new ecmp_group_id
-                            if (exists == False):
+                            if not exists:
                                 max_group_id = max_group_id + 1
                                 ecmp_group_id = max_group_id
                                 next_hops_save.append(current_next_hops_save)
@@ -155,7 +157,8 @@ class Controller(object):
                         for host in dst_dct_hosts:
                             host_ip = self.topo.get_host_ip(host) + prefix
                             # Add table entries for directly connected hosts
-                            controller.table_add("ipv4_lpm", "ecmp_group", [str(host_ip)], [str(ecmp_group_id), str(no_next_hops)])
+                            controller.table_add("ipv4_lpm", "ecmp_group", [str(host_ip)],
+                                                 [str(ecmp_group_id), str(no_next_hops)])
 
                         # Add entries in the table ecmp_group_to_nhop for every next hop
                         # Get mac addr and port for next hops
@@ -164,20 +167,20 @@ class Controller(object):
                             dst_dct_hosts = self.topo.get_hosts_connected_to(str(switches_list[dst_sw]))
                             for host in dst_dct_hosts:
                                 host_mac = self.topo.get_host_mac(host)
-                                interf_info = self.topo.get_node_intfs(fields=['node_neigh','port'])
+                                interf_info = self.topo.get_node_intfs(fields=['node_neigh', 'port'])
                                 interf_info_sw = interf_info[str(switches_list[src_sw])]
                                 for m in interf_info_sw:
-                                    if (interf_info_sw[m][0] == next_hop):
+                                    if interf_info_sw[m][0] == next_hop:
                                         next_hop_port = interf_info_sw[m][1]
                                         break
                                 # Add new ecmp_group in the table
-                                controller.table_add("ecmp_group_to_nhop", "set_nhop", [str(ecmp_group_id), str(i)], [host_mac, str(next_hop_port)])
+                                controller.table_add("ecmp_group_to_nhop", "set_nhop", [str(ecmp_group_id), str(i)],
+                                                     [host_mac, str(next_hop_port)])
 
-            print("Done with source switch: {}".format(src_sw+1))
+            print("Done with source switch: {}".format(src_sw + 1))
             print()
             print()
             src_sw = src_sw + 1
-
 
     def main(self):
         """Main function"""
